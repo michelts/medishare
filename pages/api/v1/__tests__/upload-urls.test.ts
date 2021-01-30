c75f8abe54d8351f04eac5ccfb940ccb47398fa2
Michel Sabchuk <michel@turbosys.com.br>
2021-01-30 19:06:14 -0300

    Implement the API entrypoint to get the signed post url

---
+++ pages/api/v1/__tests__/upload-urls.test.ts
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import { v1 as uuidv1 } from 'uuid';
+
+describe('upload-urls API endpoint', () => {
+  beforeAll(() => {
+    axios.defaults.adapter = require('axios/lib/adapters/http');
+  });
+
+  it('should return a signed post url to upload the file *{id}.webm*', async () => {
+    const endpoint = 'http://localhost:8000'; // compatible with s3-local
+    const bucketName = 'videos';
+    const id = uuidv1();
+
+    const { status, data } = await axios.post('http://localhost:3000/api/v1/upload-urls', { id });
+    expect(status).toEqual(200);
+    expect(data.url).toEqual(`${endpoint}/${bucketName}`);
+    expect(data.fields['Content-Type']).toEqual('video/webm');
+    expect(data.fields['bucket']).toEqual(bucketName);
+    expect(data.fields['key']).toEqual(`${id}.webm`);
+  });
+});
---
+++ pages/api/v1/upload-urls.ts
@@ -0,0 +1,36 @@
+import { NowRequest, NowResponse } from '@vercel/node'
+import AWS from 'aws-sdk';
+
+export default async function (req: NowRequest, res: NowResponse): void {
+  if(req.method === 'POST') {
+    const { id } = req.body;
+    res.status(200).json(await getPresignedPostData({ id }));
+  }
+}
+
+function getPresignedPostData({ id }) {
+  const s3 = new AWS.S3({
+    s3ForcePathStyle: true,
+    accessKeyId: 'S3RVER',
+    secretAccessKey: 'S3RVER',
+    endpoint: new AWS.Endpoint('http://localhost:8000'),
+  });
+  const params = {
+    Expires: 60,
+    Bucket: 'videos',
+    Conditions: [['content-length-range', 100, 25 * (1024 ** 2)]], // 100Byte up to 25MB
+    Fields: {
+      'Content-Type': 'video/webm',
+      key: `${id}.webm`
+    }
+  };
+  return new Promise((resolve, reject) => {
+    s3.createPresignedPost(params, (err, data) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(data);
+    });
+  });
+}
