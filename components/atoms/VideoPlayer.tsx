4b1bbc3cc09592a2d6bf2f71b648fbd759b93eaf
Michel Sabchuk <michel@turbosys.com.br>
2021-02-01 12:34:26 -0300

    Add a generic video player to be used in the ShareDetail

---
+++ components/atoms/VideoPlayer.tsx
@@ -0,0 +1,14 @@
+type VideoType = {
+  width: int,
+  height: int,
+  src: string,
+}
+
+const VideoPlayer: React.FC<VideoType> = ({ width, height, src }) => (
+  <video width={width} height={height} controls>
+    <source src={src} type="video/webm" />
+    Your browser does not support the video tag.
+  </video>
+);
+
+export default VideoPlayer;
